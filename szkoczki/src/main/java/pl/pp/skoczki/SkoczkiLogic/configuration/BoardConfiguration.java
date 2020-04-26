3360c34f8a27d65f843eac38b5793198011a62d1
517435708 <a500573435708@gmail.com>
2020-04-26 04:27:25 +0200

    implemented GUI using javaFX with communication using IoCContainers from spring boot, now after winning we can see on console this :)

--- szkoczki/src/main/java/pl/pp/skoczki/SkoczkiLogic/configuration/BoardConfiguration.java
+++ szkoczki/src/main/java/pl/pp/skoczki/SkoczkiLogic/configuration/BoardConfiguration.java
@@ -1,58 +1,140 @@
 package pl.pp.skoczki.SkoczkiLogic.configuration;
 
+import javafx.geometry.Orientation;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import pl.pp.skoczki.SkoczkiLogic.game.pawn.Pawn;
+import pl.pp.skoczki.SkoczkiLogic.game.pawn.Position;
 
-import java.util.ArrayList;
+import javax.annotation.Resource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.List;
 
-import static pl.pp.skoczki.SkoczkiLogic.game.pawn.Color.BLACK;
-import static pl.pp.skoczki.SkoczkiLogic.game.pawn.Color.WHITE;
-import static pl.pp.skoczki.SkoczkiLogic.game.pawn.Position.*;
+import static pl.pp.skoczki.SkoczkiLogic.configuration.ConfigConstants.*;
 
 @Configuration
+@DependsOn("pawnList")
 public class BoardConfiguration {
 
-    @Bean(name = "pawnList")
-    public List<Pawn> pawns() {
+    @Resource(name = "pawnList")
+    private List<Pawn> pawns;
 
-        List<Pawn> pawns = new ArrayList<>();
+    @Bean(name = "chessBoard")
+    public Scene gamePanel() throws
+                             FileNotFoundException {
 
-        pawns.add(new Pawn(WHITE, A1));
-        pawns.add(new Pawn(WHITE, B1));
-        pawns.add(new Pawn(WHITE, C1));
-        pawns.add(new Pawn(WHITE, D1));
-        pawns.add(new Pawn(WHITE, E1));
-        pawns.add(new Pawn(WHITE, F1));
-        pawns.add(new Pawn(WHITE, G1));
-        pawns.add(new Pawn(WHITE, H1));
-        pawns.add(new Pawn(WHITE, A2));
-        pawns.add(new Pawn(WHITE, B2));
-        pawns.add(new Pawn(WHITE, C2));
-        pawns.add(new Pawn(WHITE, D2));
-        pawns.add(new Pawn(WHITE, E2));
-        pawns.add(new Pawn(WHITE, F2));
-        pawns.add(new Pawn(WHITE, G2));
-        pawns.add(new Pawn(WHITE, H2));
-        pawns.add(new Pawn(BLACK, A8));
-        pawns.add(new Pawn(BLACK, B8));
-        pawns.add(new Pawn(BLACK, C8));
-        pawns.add(new Pawn(BLACK, D8));
-        pawns.add(new Pawn(BLACK, E8));
-        pawns.add(new Pawn(BLACK, F8));
-        pawns.add(new Pawn(BLACK, G8));
-        pawns.add(new Pawn(BLACK, H8));
-        pawns.add(new Pawn(BLACK, A7));
-        pawns.add(new Pawn(BLACK, B7));
-        pawns.add(new Pawn(BLACK, C7));
-        pawns.add(new Pawn(BLACK, D7));
-        pawns.add(new Pawn(BLACK, E7));
-        pawns.add(new Pawn(BLACK, F7));
-        pawns.add(new Pawn(BLACK, G7));
-        pawns.add(new Pawn(BLACK, H7));
+        ToolBar toolBar = generateToolBar();
+        Group box = board();
 
-        return pawns;
+        HBox hBox = new HBox(toolBar);
+        hBox.getChildren()
+            .add(box);
+        return new Scene(hBox, CHESSBOARD_WIDTH + TOOLBAR_WIDTH, CHESSBOARD_WIDTH);
+    }
+
+
+    @Bean(name = "select")
+    public ImageView select() throws
+                              FileNotFoundException {
+        Image image = new Image(new FileInputStream(RESOURCE_PATH + "select.png"));
+        ImageView imageView = new ImageView(image);
+        imageView.setVisible(false);
+
+        return imageView;
+    }
+
+    private Group board() throws
+                          FileNotFoundException {
+
+        ImageView[] positionImages = new ImageView[64];
+        ImageView[] boardImages = new ImageView[65];
+        ImageView[] pawnsImages = generatePawnsImages();
+
+        for (var i = 0; i < 8; i++) {
+            for (var j = 0; j < 8; j++) {
+                Image image;
+                if ((i + j) % 2 == 0) {
+                    image = new Image(new FileInputStream(RESOURCE_PATH + "darkSquare.png"));
+                } else {
+                    image = new Image(new FileInputStream(RESOURCE_PATH + "lightSquare.png"));
+                }
+                ImageView imageView = new ImageView(image);
+
+                imageView.setFitWidth(IMAGE_WIDTH);
+                imageView.setFitHeight(IMAGE_WIDTH);
+                imageView.setX(IMAGE_WIDTH * i);
+                imageView.setY(IMAGE_WIDTH * j);
+                boardImages[i * 8 + j] = imageView;
+                positionImages[i*8 + j] = Position.intToPosition(i, j).orElseThrow().getImageViewPosition();
+            }
+        }
+        boardImages[64] = select();
+
+        ImageView[] images = merge(boardImages, pawnsImages);
+        images = merge(images, positionImages);
+
+        return new Group(images);
+    }
+
+    private ImageView[] generatePawnsImages() {
+        return pawns.stream()
+                    .map(Pawn::getPawnImage)
+                    .toArray(ImageView[]::new);
+    }
+
+
+    private ImageView[] merge(ImageView[] boardImages, ImageView[] pawnsImages) {
+        ImageView[] images = new ImageView[boardImages.length + pawnsImages.length];
+
+        int i = 0;
+        for (var item : boardImages) {
+            images[i] = item;
+            i++;
+        }
+
+        for (var item : pawnsImages) {
+            images[i] = item;
+            i++;
+        }
+
+        return images;
+    }
+
+    private ToolBar generateToolBar() {
+        ToolBar toolBar = new ToolBar();
+
+        toolBar.setMinWidth(TOOLBAR_WIDTH);
+        toolBar.setMaxWidth(TOOLBAR_WIDTH);
+        toolBar.setPrefWidth(TOOLBAR_WIDTH);
+
+        Label label = new Label("New Game: ");
+        toolBar.getItems()
+               .add(label);
+
+        Button siVsSi = new Button("SI vs SI");
+        toolBar.getItems()
+               .add(siVsSi);
+
+        Button siVsHuman = new Button("SI vs Human");
+        toolBar.getItems()
+               .add(siVsHuman);
+
+        Label turn = new Label("Whose turn\n is this: ");
+        toolBar.getItems()
+               .add(turn);
+
+        toolBar.setOrientation(Orientation.VERTICAL);
+        return toolBar;
     }
 
 }
